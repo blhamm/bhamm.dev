62f6b758dfa843419c2fa6c8ef547e5b51f903bf
Brandon Hamm <brandon@structur.co>
2026-07-31 16:14:05 -0400

    Remove starter kit tests, add simple test for welcome page update alt test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,9 @@
 <?php
 
-it('returns a successful response', function () {
+it('returns a welcome page', function () {
     $response = $this->get('/');
+
+	$response->assertSee("Hey, I'm Brandon.", false);
 
     $response->assertStatus(200);
 });
