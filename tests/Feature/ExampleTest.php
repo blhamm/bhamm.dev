23e642d7f57e607875ebb43289f9761f2a2812c6
Brandon Hamm <brandon@structur.co>
2026-07-31 16:15:48 -0400

    Add assertSee for "Ship It!" Mask

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ it('returns a welcome page', function () {
     $response = $this->get('/');
 
 	$response->assertSee("Hey, I'm Brandon.", false);
+	$response->assertSee("Ship It!", false);
 
     $response->assertStatus(200);
 });
