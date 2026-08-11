0c06a27c2113ddd0212309f1d9b58f897bd28f74
Brandon Hamm <brandon@structur.co>
2026-08-10 21:47:08 -0400

    Schedule console commands for geoip geocode and download tasks

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('geoip:geocode', ['--max-mind'])->hourly();
+
+Schedule::command('geoip:download')->monthly();
+
+Schedule::command('geoip:geocode')->daily();
