c4711dc42f9140e291fd2ea13454201fee23b2c2
Brandon Hamm <brandon@structur.co>
2026-08-10 22:05:51 -0400

    Fix modal interpolation issue in if statement

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -8,7 +8,7 @@
 
 <flux:modal 
     name="{{ $name }}" 
-    @if($show) x-init="$nextTick(() => $flux.modal('{{ $name }}').show())" @endif
+    :x-init="$show ? '$nextTick(() => $flux.modal(\'' . $name . '\').show())' : null"
     class="w-full h-dvh p-0 m-0 overflow-hidden bg-transparent border-none outline-none max-h-none max-w-none" 
     variant="bare"
     x-on:modal-show.document="window.lenis?.stop()"
